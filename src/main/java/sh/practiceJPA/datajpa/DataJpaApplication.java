cb85372815ad294b86d9a174dc2f529e780a2b07
YSH <sheepsh93@gmail.com>
2021-01-31 21:40:25 +0900

    공통 인터페이스 설정

--- src/main/java/sh/practiceJPA/datajpa/DataJpaApplication.java
+++ src/main/java/sh/practiceJPA/datajpa/DataJpaApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@EnableJpaRepositories(basePackages = "sh.practiceJPA.datajpa.repository")// 원래는 이렇게 위치를 잡아줘야 하는데,
+//스프링 부트를 쓰면 필요없다.
 public class DataJpaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DataJpaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DataJpaApplication.class, args);
+    }
 
 }
