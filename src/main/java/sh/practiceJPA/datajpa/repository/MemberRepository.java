2d1a3acd8ea213f8b5e7796538c2fab61f4d661a
YSH <sheepsh93@gmail.com>
2021-01-31 21:53:05 +0900

    공통 인터페이스 적용

--- src/main/java/sh/practiceJPA/datajpa/repository/MemberRepository.java
+++ src/main/java/sh/practiceJPA/datajpa/repository/MemberRepository.java
@@ -3,5 +3,6 @@ package sh.practiceJPA.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sh.practiceJPA.datajpa.entity.Member;
 
+//JpaRepository<Entity,Entity에 맵핑된 pk>
 public interface MemberRepository  extends JpaRepository<Member,Long> {
 }
