418c56c317de1807fb41d5235670a05aafd8bed3
YSH <sheepsh93@gmail.com>
2021-01-31 20:56:44 +0900

    예제 도메인 모델과 동작확인

--- src/main/java/sh/practiceJPA/datajpa/entity/Member.java
+++ src/main/java/sh/practiceJPA/datajpa/entity/Member.java
@@ -1,27 +1,48 @@
 package sh.practiceJPA.datajpa.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) //아래 인자 없는 생성자 대신 이거..
+@ToString(of = {"id","username","age"})
 public class Member {
 
     @Id
     @GeneratedValue
+    @Column(name ="member_id")
     private Long id;
 
     private String username;
 
-    protected Member() {
-    }
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tema_id")
+    private Team team;
+
+
 
     public Member(String username) {
         this.username = username;
     }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            this.team = team;
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+
+    //    protected Member() {
+//    }
 }
