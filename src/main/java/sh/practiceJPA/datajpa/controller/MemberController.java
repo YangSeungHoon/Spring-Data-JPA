5240f837f7770a8521124522b60952ba773e3f3f
YSH <sheepsh93@gmail.com>
2021-02-06 08:57:01 +0900

    Web 확장 - 페이징 처리.

--- src/main/java/sh/practiceJPA/datajpa/controller/MemberController.java
+++ src/main/java/sh/practiceJPA/datajpa/controller/MemberController.java
@@ -1,6 +1,8 @@
 package sh.practiceJPA.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,13 +23,29 @@ public class MemberController {
         return member.getUsername();
     }
 
+    //이렇게 페이지 인터페이스만 파라미터로 받게하면 알아서 페이징 처리해준다.
+    //8080/members?page=0 => 0페이지에서 id를 20개 꺼낸다.
+    //8080/members?page=0&size=3 => 0페이지에서 3개만 꺼낸다.
+    @GetMapping("/members")
+    public Page<Member> list(Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);
+        return page;
+    }
+
     @GetMapping("/members2/{id}") //도메인 컨버터가 객체를 @PathVariable로 받아와도 알아서 해결해준다.
     public String findMember(@PathVariable("id") Member member){
         return member.getUsername();
     }
 
-    @PostConstruct
-    public void init() {
-        memberRepository.save(new Member("userA"));
+//    @PostConstruct
+//    public void init() {
+//        memberRepository.save(new Member("userA"));
+//    }
+
+    @PostConstruct//page 실험용 데이터터
+   public void init() {
+        for(int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user"+ i,i));
+        }
     }
 }
