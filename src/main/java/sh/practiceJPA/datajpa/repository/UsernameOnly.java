0f9d54d1225fdcf355318203b2cc0899f0729719
YSH <sheepsh93@gmail.com>
2021-02-07 19:06:13 +0900

    Interface 기반의 Open Projection

--- src/main/java/sh/practiceJPA/datajpa/repository/UsernameOnly.java
+++ src/main/java/sh/practiceJPA/datajpa/repository/UsernameOnly.java
@@ -1,7 +1,12 @@
 package sh.practiceJPA.datajpa.repository;
 
 
+import org.springframework.beans.factory.annotation.Value;
+
 public interface UsernameOnly {
 
+    //다음과 같이 설정하여 Open Projection을 하면 먼저 해당 객체를 가져오고 그다음 @Value어노테이션의 조건을 맞춰서 다시 원하는 데이터를
+    //찍어서 계산한다.
+    @Value("#{target.username + ' ' + target.age}")
     String getUsername();
 }
